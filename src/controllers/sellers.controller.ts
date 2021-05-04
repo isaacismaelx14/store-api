b83a3addec0fa3861cae50c79ae930d0c1d51f5a
Isaac Martinez <isaacismael789@gmail.com>
2021-05-04 15:38:27 -0400

    Auth for Sellers

--- src/controllers/sellers.controller.ts
+++ src/controllers/sellers.controller.ts
@@ -1,6 +1,7 @@
 import Database, { dataResponse, options, queryParam } from '../database';
 import dbConfig from '../db.config';
 import Errors from '../database/messages/errors';
+import JwtController from '../function/jws';
 
 interface seller {
   id?: number;
@@ -14,6 +15,7 @@ interface seller {
 
 const db = new Database('sellers', dbConfig);
 const errors = new Errors();
+const jwtCtrl = new JwtController();
 
 const sellerData = (Seller: seller): queryParam => [
     { selector: 'user_id', value: Seller.user_id },
@@ -29,23 +31,38 @@ class SellersController {
         return await db.select('*', id ? options : null);
     }
 
-    async post(Seller: seller): Promise<dataResponse> {
+    async post(Seller: seller, auth?:string): Promise<dataResponse> {
         const {description, direction, name, rank, user_id} = Seller;
-        if(description && direction && name && rank !== undefined && user_id)
-            return await db.insert(sellerData(Seller));
-        else return errors.allNeeded;
+        if(auth && await jwtCtrl.checkToken(auth, {id:user_id})){
+            if(description && direction && name && rank !== undefined && user_id)
+                return await db.insert(sellerData(Seller));
+            else return errors.allNeeded;
+        }
+        return errors.notAuth;
     }
 
-    async update(id: number, Seller: seller): Promise<dataResponse> {
+    async update(id: number, Seller: seller, auth?:string): Promise<dataResponse> {
         if(Seller.id || Seller.user_id) return errors.idCannotChange;
-        const {description, direction, name, rank} = Seller;
-        if(description || direction || name || rank !== undefined )
-            return await db.update(sellerData(Seller), { selector: 'id', value: id });
-        else return errors.requestEmpty;
+        if(auth && await jwtCtrl.checkToken(auth, {id:await this.getUserId(id)})){
+            const {description, direction, name, rank} = Seller;
+            if(description || direction || name || rank !== undefined )
+                return await db.update(sellerData(Seller), { selector: 'id', value: id });
+            else return errors.requestEmpty;
+        }
+        return errors.notAuth;
     }
 
-    async delete(id: number): Promise<dataResponse> {
-        return await db.delete({ selector: 'id', value: id });
+    async delete(id: number, auth?:string): Promise<dataResponse> {
+        if(auth && await jwtCtrl.checkToken(auth, {id:await this.getUserId(id)}))
+            // return await db.delete({ selector: 'id', value: id });
+            return {code:201, data:'deleted'};
+        return errors.notAuth;
+    }
+
+    private async getUserId(sellerId:number){
+        const req =await this.get(sellerId);
+        const {user_id} = req.data;
+        return user_id;
     }
 }
 
--- src/router/sellers.router.ts
+++ src/router/sellers.router.ts
@@ -7,31 +7,35 @@ router.use(express.json());
 
 
 router.get('/', async(_, res:Response) => {
-  const {code, data} = await sellerCtrl.get();
-  res.status(code).json(data);
+    const {code, data} = await sellerCtrl.get();
+    res.status(code).json(data);
 });
 
 router.get('/:id', async(req:Request, res:Response) => {
-  const id = parseInt(req.params.id);
-  const {code, data} = await sellerCtrl.get(id);
-  res.status(code).json(data);
+    const id = parseInt(req.params.id);
+    const {code, data} = await sellerCtrl.get(id);
+    res.status(code).json(data);
 });
 
 router.post('/', async(req:Request, res:Response) => {
-  const {code, data} = await sellerCtrl.post(req.body);
-  res.status(code).json(data);
+    const auth = req.get('authorization');
+    const {code, data} = await sellerCtrl.post(req.body, auth);
+    res.status(code).json(data);
 });
 
 router.patch('/:id', async(req:Request, res:Response) => {
-  const id = parseInt(req.params.id);
-  const {code, data} = await sellerCtrl.update(id, req.body);
-  res.status(code).json(data);
+    const auth = req.get('authorization');
+    const id = parseInt(req.params.id);
+    const {code, data} = await sellerCtrl.update(id, req.body, auth);
+    res.status(code).json(data);
 });
 
 router.delete('/:id', async(req:Request, res:Response) => {
-  const id = parseInt(req.params.id);
-  const {code, data} = await sellerCtrl.delete(id);
-  res.status(code).json(data);
+    const auth = req.get('authorization');
+    const id = parseInt(req.params.id);
+    const {code, data} = await sellerCtrl.delete(id, auth);
+    res.status(code).json(data);
 });
+
 
 export default router;
